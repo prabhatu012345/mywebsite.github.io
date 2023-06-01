f59ccdecf15b88f9012f61662ce0af1337b0b770
Prabhat GitHub <prabhatu012345@gail.com>
2023-06-01 12:57:37 +0530

    Pushing file

---
+++ script.sh
@@ -0,0 +1,7 @@
+echo "# mywebsite.github.io" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/prabhatu012345/mywebsite.github.io.git
+git push -u origin master
